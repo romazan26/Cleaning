4e83074dc0404cdd1ab257646633d1224f5547a0
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-12-15 11:22:22 +0300

    Initial Commit

---
+++ Cleaning/CleaningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleaningApp.swift
+//  Cleaning
+//
+//  Created by Роман on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CleaningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cleaning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cleaning
+//
+//  Created by Роман on 15.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
